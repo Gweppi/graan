5c3ee3c42f748e1629f609fac066f6ebf7e71750
Gweppi <dev@jesperdinger.com>
2023-10-14 10:54:27 +0200

    new

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from dotenv import load_dotenv
+""" main """
 import os
+import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
-password = os.environ.get('PASSWORD')
+def get_access_token(username: str, password: str) -> str:
+    """ Function obtaining access token """
+    data = {
+        "client_id": "cdse-public",
+        "username": username,
+        "password": password,
+        "grant_type": "password",
+    }
+    try:
+        r = requests.post(
+            "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token",
+            data=data,
+            timeout=10
+        )
+        r.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        raise Exception(f"Access token creation failed. Response from the server was: {r.json()}") from e
+    return r.json()["access_token"]
 
-api = SentinelAPI('jeppi', password)
+usernameEnv = os.environ.get("USERNAME")
+passwordEnv = os.environ.get("PASSWORD")
 
-product = os.environ.get('PRODUCT')
+access_token = get_access_token(usernameEnv, passwordEnv)
 
-data = api.get_product_odata(product)
-
-api.download(product, nodefilter="*B02")
+print(access_token)
