ad3a5822ff104ba5d9a2b72ad757860ea51017a2
Jesper Dinger <78754860+Gweppi@users.noreply.github.com>
2024-03-14 20:56:35 +0000

    nuusbest

--- filter.py
+++ filter.py
@@ -3,8 +3,8 @@ import urllib.request
 import openeo
 import datetime
 
-start_date = '2022-09-01T00:00:00.000Z'
-end_date = '2022-10-31T23:59:59.999Z'
+start_date = '2022-11-11T00:00:00.000Z'
+end_date = '2023-08-11T23:59:59.999Z'
 
 def short_date(date: str):
   return date[0:10].replace('-', '')
@@ -28,7 +28,7 @@ with urllib.request.urlopen(f'https://www.daggegevens.knmi.nl/klimatologie/dagge
 
 # Filter dates
 dates = []
-sat_url = f'https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?cloudCover=[0,10]&startDate={start_date}&completionDate={end_date}&sortParam=startDate&geometry=POLYGON ((5.554850262125484 52.53996513567034, 5.555136972613241 52.5355639065524, 5.559451282798307 52.535622038868866, 5.559314753995039 52.54002326215965, 5.554850262125484 52.53996513567034))'.replace(' ', '%20')
+sat_url = f'https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?cloudCover=[0,30]&startDate={start_date}&completionDate={end_date}&sortParam=startDate&geometry=POLYGON ((5.554850262125484 52.53996513567034, 5.555136972613241 52.5355639065524, 5.559451282798307 52.535622038868866, 5.559314753995039 52.54002326215965, 5.554850262125484 52.53996513567034))'.replace(' ', '%20')
 with urllib.request.urlopen(sat_url) as f:
     sat_data = json.load(f)
 
